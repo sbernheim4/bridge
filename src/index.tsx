e5f51830d0169f2dc060bad6a7f7327bbfd59783
sbernheim4 <sambernheim@gmail.com>
2020-05-19 11:39:28 -0400

    feat: demo f# in tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ import ReactDOM from "react-dom";
 
 import { BrowserRouter } from 'react-router-dom'
 
+
+
+
+
+// eslint-disable-next-line
+// @ts-ignore
+import { subtract, subtractFive } from './f-sharp/src/AppTwo.fs';
+// eslint-disable-next-line
+// @ts-ignore
+import { determineTrickPoints } from './f-sharp/src/App.fs';
+
+
+console.log(`10 - 5 is ${subtractFive(10)}`);
+console.log(`20 - 3 is ${subtract(3, 20)}`);
+console.log(`determine trick points with suit index of 2 and level of 4 is
+    ${determineTrickPoints(2, 4)}
+`);
+
+
+
+
+
 import "./styles/reset.css";
 
 /* App is the entry point to the React code.*/
