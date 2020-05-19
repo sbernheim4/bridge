6ae380b7548f3ce5f0dbfca3e416ec4ba096e457
sbernheim4 <sambernheim@gmail.com>
2020-05-19 00:22:55 -0400

    feat: write point system in f#

--- src/f-sharp/src/App.fs
+++ src/f-sharp/src/App.fs
@@ -1,11 +1,50 @@
 module App
 
-let add x y = x + y
-let addTwo = add 2
+open netstandard
 
-type Bid = {
-    suitIndex: int;
-    level: int
+type contractMade = {
+    aboveTheLine: int
+    belowTheLine: int
 }
 
-let determineTrickPoints bid =
+let determineTrickPoints suitIndex level =
+    let isNoTrump = if suitIndex = 0 then true else false
+    let isMajor = if suitIndex <= 2 then true else false
+
+    if isNoTrump then 40 + (level - 1) * 30
+    elif isMajor then level * 30
+    else level * 20
+
+let determineBounsPoints trickCount suitIndex level isPenaltyDoubled =
+    let determineSlamBonus level =
+        if level = 6 then 500
+        elif level = 7 then 1000
+        else 0
+
+    let doubleMultiplier = if isPenaltyDoubled then 2 else 1
+    let suitMultiplier = if suitIndex <= 2 then 30 else 20
+    let slamBonus = determineSlamBonus level
+    let overTricks = trickCount - level
+    ((suitMultiplier * overTricks * doubleMultiplier) + slamBonus);
+
+let determineNegativePoints trickCount contractLevel isPenaltyDoubled =
+    let underTrickValue = 50
+    let multiplier = if isPenaltyDoubled then 2 else 1
+    (-1 * (contractLevel - trickCount) * 50 * multiplier)
+
+let checkContractMade trickCount suitIndex level isPenaltyDoubled =
+    if trickCount >= level then
+        let overTricks = determineBounsPoints trickCount suitIndex level isPenaltyDoubled
+        let underTricks = determineTrickPoints suitIndex level
+
+        {
+            aboveTheLine=overTricks;
+            belowTheLine=underTricks
+        }
+    else
+        let overTricks = determineNegativePoints trickCount level isPenaltyDoubled
+
+        {
+            aboveTheLine=overTricks
+            belowTheLine=0
+        }
